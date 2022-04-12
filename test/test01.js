dbef061330827fbc9aacdf2ed7c904b577d0056b
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-12 12:39:45 +0200

    feat: Ejercicio completado

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,21 @@
 function geekshubs(index) {
-    return "";
+
+    let resultado = "";
+    for (let i = 1; i <= index; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            resultado += "GeeksHubs\n";
+
+        } else if (i % 3 === 0) {
+            resultado += "Geeks\n";
+
+        } else if (i % 5 === 0) {
+            resultado += "Hubs\n";
+            
+        } else {
+            resultado += i + "\n";
+        }
+    }
+
+    return resultado;
 }
 module.exports = geekshubs;
